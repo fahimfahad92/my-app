5fbfbbe8bc658a5c0a5e672b705d42499f3fd9e7
fahimfahad92 <fahim.fahad@toptal.com>
2025-06-24 20:30:36 +0600

    Update weather details response type to exclude unused fields

--- src/app/weather/types/weather-types.ts
+++ src/app/weather/types/weather-types.ts
@@ -57,13 +57,13 @@ export interface WeatherDetailResponse {
 
 export interface Location {
   name: string;
-  region: string;
+  // region: string;
   country: string;
-  lat: number;
-  lon: number;
+  // lat: number;
+  // lon: number;
   tz_id: string;
-  localtime_epoch: number;
-  localtime: string; // format "YYYY-MM-DD HH:mm"
+  // localtime_epoch: number;
+  localtime: string;
 }
 
 export interface Forecast {
@@ -72,10 +72,10 @@ export interface Forecast {
 
 export interface ForecastDay {
   date: string; // format "YYYY-MM-DD"
-  date_epoch: number;
   day: DaySummary;
   astro: Astro;
   hour: Hourly[];
+  // date_epoch: number;
 }
 
 export interface DaySummary {
@@ -83,22 +83,22 @@ export interface DaySummary {
   maxtemp_f: number;
   mintemp_c: number;
   mintemp_f: number;
-  avgtemp_c: number;
-  avgtemp_f: number;
-  maxwind_mph: number;
-  maxwind_kph: number;
-  totalprecip_mm: number;
-  totalprecip_in: number;
-  totalsnow_cm: number;
-  avgvis_km: number;
-  avgvis_miles: number;
-  avghumidity: number;
-  daily_will_it_rain: number;
-  daily_chance_of_rain: number;
-  daily_will_it_snow: number;
-  daily_chance_of_snow: number;
   condition: Condition;
-  uv: number;
+  // avgtemp_c: number;
+  // avgtemp_f: number;
+  // maxwind_mph: number;
+  // maxwind_kph: number;
+  // totalprecip_mm: number;
+  // totalprecip_in: number;
+  // totalsnow_cm: number;
+  // avgvis_km: number;
+  // avgvis_miles: number;
+  // avghumidity: number;
+  // daily_will_it_rain: number;
+  // daily_chance_of_rain: number;
+  // daily_will_it_snow: number;
+  // daily_chance_of_snow: number;
+  // uv: number;
 }
 
 export interface Astro {
@@ -106,49 +106,49 @@ export interface Astro {
   sunset: string;
   moonrise: string;
   moonset: string;
-  moon_phase: string;
-  moon_illumination: number;
+  // moon_phase: string;
+  // moon_illumination: number;
 }
 
 export interface Hourly {
-  time_epoch: number;
   time: string; // "YYYY-MM-DD HH:mm"
   temp_c: number;
-  temp_f: number;
-  is_day: 0 | 1;
-  condition: Condition;
-  wind_mph: number;
-  wind_kph: number;
-  wind_degree: number;
-  wind_dir: string;
-  pressure_mb: number;
-  pressure_in: number;
-  precip_mm: number;
-  precip_in: number;
-  snow_cm: number;
-  humidity: number;
-  cloud: number;
-  feelslike_c: number;
-  feelslike_f: number;
-  windchill_c: number;
-  windchill_f: number;
-  heatindex_c: number;
-  heatindex_f: number;
-  dewpoint_c: number;
-  dewpoint_f: number;
-  will_it_rain: number;
-  chance_of_rain: number;
-  will_it_snow: number;
-  chance_of_snow: number;
-  vis_km: number;
-  vis_miles: number;
-  gust_mph: number;
-  gust_kph: number;
-  uv: number;
+  // time_epoch: number;
+  // temp_f: number;
+  // is_day: 0 | 1;
+  // condition: Condition;
+  // wind_mph: number;
+  // wind_kph: number;
+  // wind_degree: number;
+  // wind_dir: string;
+  // pressure_mb: number;
+  // pressure_in: number;
+  // precip_mm: number;
+  // precip_in: number;
+  // snow_cm: number;
+  // humidity: number;
+  // cloud: number;
+  // feelslike_c: number;
+  // feelslike_f: number;
+  // windchill_c: number;
+  // windchill_f: number;
+  // heatindex_c: number;
+  // heatindex_f: number;
+  // dewpoint_c: number;
+  // dewpoint_f: number;
+  // will_it_rain: number;
+  // chance_of_rain: number;
+  // will_it_snow: number;
+  // chance_of_snow: number;
+  // vis_km: number;
+  // vis_miles: number;
+  // gust_mph: number;
+  // gust_kph: number;
+  // uv: number;
 }
 
 export interface Condition {
   text: string;
-  icon: string; // URL
+  icon: string;
   code: number;
 }
